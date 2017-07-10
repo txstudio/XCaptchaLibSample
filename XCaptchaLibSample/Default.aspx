415c3503efd6a6021df7786dc1ca6a122901fed1
Shi Hao Wang <st890409@hotmail.com>
2017-07-10 15:50:41 +0800

    XCaptcha 套件使用範例
    再 WebForm 專案使用 XCaptcha 套件加入隨機驗證碼認證。

---
+++ XCaptchaLibSample/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="XCaptchaLibSample.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style>
+        body {
+            line-height:1.5em;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>
+            此範例程式碼為使用 XCaptcha 進行驗證碼認證
+        </p>
+
+        <asp:Image runat="server" ID="CaptchaImage" />
+        <div>請輸入圖形驗證碼：</div>
+        <asp:TextBox runat="server" ID="CaptchaTextBox"></asp:TextBox>
+
+        <hr />
+
+        <asp:Button runat="server" ID="CaptchaButton" Text="驗證" OnClick="CaptchaButton_Click"/>
+        <br />
+        驗證結果：<asp:Literal runat="server" ID="Validator"></asp:Literal>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ XCaptchaLibSample/Default.aspx.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XCaptchaLibSample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        const string captcha_session_name = "captcha_session_name";
+        const string base64_image_format = "data:image/jpg;base64,{0}";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(this.IsPostBack == false)
+            {
+                //隨機取得驗證碼數值
+                var _captcha = this.GetCaptcha();
+
+                //建立 XCaptcha 驗證物件
+                var _builder = new XCaptcha.ImageBuilder();
+                var _result = _builder.Create(_captcha);
+
+                //取得驗證碼數值圖片
+                var _buffer = _result.Image;
+                var _base64 = this.GetBase64Image(_buffer);
+
+                this.SetCaptchValue(_captcha);
+                this.CaptchaImage.ImageUrl = _base64;
+            }
+        }
+
+        protected void CaptchaButton_Click(object sender, EventArgs e)
+        {
+            var _captcha = this.CaptchaTextBox.Text;
+
+            if(this.IsCaptchaValidator(_captcha) == true)
+                this.Validator.Text = "驗證通過";
+            else
+                this.Validator.Text = "驗證失敗";
+
+        }
+
+        /// <summary>設定驗證碼</summary>
+        private void SetCaptchValue(string captcha)
+        {
+            this.Session[captcha_session_name] = captcha;
+        }
+
+        /// <summary>進行驗證碼的檢查</summary>
+        private bool IsCaptchaValidator(string captcha)
+        {
+            if(this.Session[captcha_session_name] == null)
+                return false;
+
+            var _imgCaptcha = this.Session[captcha_session_name].ToString();
+
+            if (String.Equals(_imgCaptcha, captcha, StringComparison.OrdinalIgnoreCase) == true)
+                return true;
+
+            return false;
+        }
+
+
+        /// <summary>取得 base64 編碼影像</summary>
+        private string GetBase64Image(byte[] content)
+        {
+            var _quotationMark = new char[]{'"'};
+            var _json = JsonConvert.SerializeObject(content);
+            var _base64 = _json.Trim(_quotationMark);
+
+            return string.Format(base64_image_format, _base64);
+        }
+
+        /// <summary>使用 guid 取得隨機的五個字元英數字</summary>
+        private string GetCaptcha()
+        {
+            var _guid = Guid.NewGuid();
+            var _guidString = _guid.ToString();
+            var _captcha = _guidString.Substring(0, 5);
+
+            return _captcha;
+        }
+    }
+}
